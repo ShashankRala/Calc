f162e22108f4d5d7be7d2a14b1ba0b1e347fb546
ShashankRala <ralashashankyadav0402@gmail.com>
2024-10-02 23:35:26 +0530

    Sessions,APIs are pending

---
+++ src/main/java/mypackage/MyServlet.java
@@ -0,0 +1,68 @@
+package mypackage;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String[] jokes= {
+			"Client: \"Can you make the website faster?\"\r\n"
+			+ "Developer: Sure, let me just remove all the features you asked for.",
+			"Why do programmers prefer dark mode?\r\n"
+			+ "Because the light attracts bugs!",
+			"Why did the developer break up with their girlfriend?\r\n"
+			+ "They had too many arguments.",
+			"I told my computer I needed a break, and now it won’t stop sending me\r\n"
+			+ "“404: Break not found” errors!"
+	};
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String str1 = request.getParameter("num1");
+		String str2 = request.getParameter("num2");
+		String str3 = request.getParameter("bt1");
+		//type casting
+		int a = Integer.parseInt(str1);
+		int b = Integer.parseInt(str2);
+		
+		//int sum = num1 + num2;
+		
+		int ans = 0;
+		if(str3.equals("1")) ans=a+b;
+		else if(str3.equals("2")) ans=a-b;
+		else if(str3.equals("3")) ans=a*b;
+		else  ans=a/b;
+//		//response.getWriter().append("Ans = "+ans);
+		
+		int randomIndex= (int) (Math.random() * jokes.length);
+		String randomJoke = jokes[randomIndex];
+		response.sendRedirect("NewFile.jsp?ans="+ans+"&joke="+randomJoke);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/NewFile.jsp
@@ -0,0 +1,62 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Calc JSP Page</title>
+    <style>
+    h3{
+    color:white;
+    }
+        img {
+            height: 9%;
+            width: 30%;
+        }
+        body {
+            text-align: center;
+            background-image: url('images/background.jpg'); /* Using relative path */
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+        }
+        
+        input, button {
+   			font-size:30px;
+   			color:#D4AF37;
+   			background-color:black;
+   			border: 2px solid #D4AF37;
+   			border-radius:10%;     
+        }
+        
+        img{
+			width: 20%;
+			height:20%;
+			margin-top: 25px;
+			border-radius:5%;
+			}
+		img:hover{
+ 		 -ms-transform: scale(1.1); /* IE 9 */
+  		 -webkit-transform: scale(1.1); /* Safari 3-8 */
+  		  transform: scale(1.1); 
+}
+        
+    </style>
+</head>
+<body>   
+    <img src="images/laughing.gif" alt="laugh emoji GIF"> <!-- Relative path for the image -->
+    <h1>Joke, Ok The Day!</h1>
+    <h3>😊<%=request.getParameter("joke") %></h3>
+    
+    <form action="MyServlet">
+    <input name="num1" placeholder="First Number"></input>
+    <input name="num2" placeholder="Second Number"></input>
+    
+    <button name="bt1" value="1">+</button>
+    <button name="bt1" value="2">-</button>
+    <button name="bt1" value="3">x</button>
+    <button name="bt1" value="4">/</button>
+    </form>
+    <h1>Ans=<%=request.getParameter("ans") %></h1>
+</body>
+</html>
